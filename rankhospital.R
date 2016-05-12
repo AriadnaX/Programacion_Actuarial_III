2794f3f06d6d1243de5dd1dd5c3995b316853b57
Ariadna Xilonen Velazquez <Ariadna.xilonen@gmail.com>
2016-05-11 20:31:55 -0500

    Calidad de hospitales

---
+++ rankhospital.R
@@ -0,0 +1,52 @@
+
+
+rankhospital <- function(estado, resultado, num="mejor"){
+  
+  data <- read.csv("outcome-of-care-measures.csv")
+  C <- FALSE
+  
+  
+  if(resultado== "ataque"){
+    col <- 11
+  }
+  else if (resultado== "falla"){
+    col <- 17
+  }
+  else {
+    col <- 23
+  }
+  
+  a <- matrix(data[,col],4706,1)
+  data[,col] <- suppressWarnings(as.numeric(a))
+  data[,2] <- as.character(data[,2])
+  
+  b <- data[grep(estado,data$State),]
+  
+  Orden <- b[order(b[,col], b[,2], na.last=NA),]
+  
+  if (num == "mejor"){
+    r <- Orden[1,2]
+  } else if (num == "peor"){
+    r <- Orden[nrow(Orden),2]
+  } else {
+    r <- Orden[num,2]
+  }
+  
+  if (!((resultado == "ataque") | (resultado == "falla")
+        | (resultado == "neumonia"))) {
+    stop ("resultado inválido")
+  }
+  
+  e <- matrix(data[,7], 4706,1)
+  
+  for (i in 1:length(e))
+    if(estado == e[i]){
+    C <- TRUE
+    }
+   if(!C){
+     stop("Estado inválido")
+   }
+  r
+
+  
+}
